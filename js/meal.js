f9041f82d3ee8891328d98b0aa27421f50a612ab
Arafat817 <gr1arafat@gmail.com>
2023-03-26 01:24:46 +0600

    modal uses

--- js/meal.js
+++ js/meal.js
@@ -1,28 +1,31 @@
-const loadMeals = (searchText)=>{
+const loadMeals = (searchText) => {
   const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
-  console.log(url)  
+  console.log(url)
   fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals));
 
 }
-const displayMeals = meals =>{
-   // console.log(meals);
-   //step-1 : container element
-   const mealsContainer = document.getElementById('meals-container');
-   mealsContainer.innerText ='';
-   meals.forEach (meal =>{
+const displayMeals = meals => {
+  // console.log(meals);
+  //step-1 : container element
+  const mealsContainer = document.getElementById('meals-container');
+  mealsContainer.innerText = '';
+  meals.forEach(meal => {
     console.log(meal)
     //step-2: for create child for each element
     const mealDiv = document.createElement('div');
     mealDiv.classList.add('col');
     //step-3 : set content of the chield 
-    mealDiv.innerHTML =`
+    mealDiv.innerHTML = `
     <div class="card h-100">
                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
                     <div class="card-body">
                       <h5 class="card-title">${meal.strMeal}</h5>
                       <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                      <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetais"> 
+                      Details
+                      </button>
                     </div>
                   </div>
     `
@@ -31,12 +34,26 @@ const displayMeals = meals =>{
     mealsContainer.appendChild(mealDiv);
 
 
-   })
+  })
 }
-const searchMeals = () =>{
+const searchMeals = () => {
   const searchText = document.getElementById('search-field').value;
   console.log(searchText);
   loadMeals(searchText);
+}
+
+const loadMealDetail = idMeal => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayMealsDetails(data.meals[0]));
+}
+const displayMealsDetails = meal => {
+  document.getElementById('mealDetaisLabel').innerText = meal.strMeal;
+  const mealDetais =document.getElementById('mealDetaisBody');
+  mealDetais.innerHTML =`
+  <img class="img-fluid" src="${meal.strMealThumb}">
+  `
 }
 
 loadMeals('rice');
